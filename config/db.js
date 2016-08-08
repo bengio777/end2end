16868b354fa38e377aabe8011df4a9da04957ba9
Alexander Murphy <amurphy345@gmail.com>
2016-08-08 01:22:54 -0600

    added knex env

--- config/db.js
+++ config/db.js
@@ -2,6 +2,9 @@ var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile.js')[environment];
 // bookshelf = require('bookshelf')(config);
 // module.exports = require('knex')(config);
+var environment = process.env.NODE_ENV || 'development';
+var config = require('../knexfile.js')[environment];
+
 var knex = require('knex')(config)
 module.exports = {
   knex: knex,
