bfdb628c2fc95f4705b1fd753ea9efd67228b4be
Alexander Murphy <amurphy345@gmail.com>
2016-08-08 01:34:15 -0600

    i am not good at this

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
-
+console.log(process.env.NODE_ENV)
 var environment = process.env.NODE_ENV || 'development';
-var config = require('../knexfile.js')[environment];
+var config = require('../knexfile.js')['production'];
 
 var knex = require('knex')(config)
 module.exports = {
