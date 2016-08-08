e8367e35b4890d46ab84bd6c0af047000f0f0b5a
Alexander Murphy <amurphy345@gmail.com>
2016-08-08 09:50:57 -0600

    woot 2

--- controllers/auth/passport_strategies.js
+++ controllers/auth/passport_strategies.js
@@ -12,8 +12,8 @@ var e2eUserTable = ModelBase.extend({
 passport.use(new uberStrategy({
     clientID: process.env.uber_api_client_id,
     clientSecret: process.env.uber_api_client_secret,
-    // callbackURL    : "https://e2etravel.herokuapp.com/auth/uber/callback",
-    callbackURL: "http://localhost:3000/auth/uber/callback",
+    callbackURL    : "https://e2etravel.herokuapp.com/auth/uber/callback",
+    // callbackURL: "http://localhost:3000/auth/uber/callback",
     state: true
   },
   (accessToken, refreshToken, profile, done) => {
