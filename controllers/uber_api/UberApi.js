77c3de3b9c0e53c9dd3a71f6d5ba4facdc020482
Alexander Murphy <amurphy345@gmail.com>
2016-08-19 21:26:40 -0600

    about to rewrite history

--- controllers/uber_api/UberApi.js
+++ controllers/uber_api/UberApi.js
@@ -1,15 +1,15 @@
-// require('dotenv').config({path:'../../.env'})
 var Promise = require('bluebird')
 var Uber = Promise.promisifyAll(require('node-uber'))
 var uber = new Uber({
     client_id: process.env.uber_api_client_id,
     client_secret: process.env.uber_api_client_secret,
     server_token: process.env.uber_api_server_token,
-    redirect_uri: 'http://localhost:3000/auth/uber/callback',
+    redirect_uri: '/auth/uber/callback',
     name: 'e2e',
     language: 'en_US', // optional, defaults to en_US
     sandbox: true // optional, defaults to false
 })
+
 Promise.promisifyAll(uber)
 Promise.promisifyAll(uber.estimates)
 Promise.promisifyAll(uber.products)
